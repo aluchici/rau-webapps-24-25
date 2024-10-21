038b55d68f14a161eec75cc1265313e65ecce71c
Niya-03 <niya.dimitrova.2016@gmail.com>
2024-10-21 13:50:29 +0300

    Formatting

--- CSE/dimitrova-niya/test/test.js
+++ CSE/dimitrova-niya/test/test.js
@@ -10,6 +10,8 @@ let input = document.createElement('input');
 input.type = 'numeric';
 document.body.appendChild(input);
 
+
+
 let btn = document.createElement('button');
 btn.textContent = 'Click me';
 btn.addEventListener('click', oddOrEven)
