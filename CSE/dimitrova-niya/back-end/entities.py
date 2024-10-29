b3b4f9de49f48caf148b6d900f5b31a19f8a5925
Niya-03 <niya.dimitrova.2016@gmail.com>
2024-10-29 17:33:38 +0200

    Added incorrect function

--- CSE/dimitrova-niya/back-end/entities.py
+++ CSE/dimitrova-niya/back-end/entities.py
@@ -13,6 +13,7 @@ class User:
         self.password = None 
         self.created_at = None 
         self.updated_at = None 
+        self.is_active = None
     
     def is_password_valid(self):
         if self.password is None or len(self.password) < 8:
@@ -30,7 +31,8 @@ class User:
             email, 
             password, 
             created_at, 
-            updated_at
+            updated_at,
+            is_active
         ) 
         VALUES (
             '{self.first_name}', 
@@ -41,13 +43,61 @@ class User:
             '{self.email}', 
             '{self.password}', 
             {self.created_at}, 
-            {self.updated_at}
+            {self.updated_at},
+            {self.is_active}
         );""" 
         cursor = dbconnection.cursor()
         cursor.execute(query)
         dbconnection.commit()
         cursor.close()
         dbconnection.close()
+
+    def from_tuple(self, user_tuple):
+        self.id = user_tuple[0]
+        self.first_name = user_tuple[1]
+        self.last_name = user_tuple[2]
+        self.dob = user_tuple[3]
+        self.gender = user_tuple[4]
+        self.phone = user_tuple[5]
+        self.email = user_tuple[6]
+        self.password = user_tuple[7]
+        self.created_at = user_tuple[8]
+        self.updated_at = user_tuple[9]
+        self.is_active = user_tuple[10]
+        
+        return self
+    
+    def from_dict(self, user_dict):
+        self.id = user_dict.get["id"]
+        self.first_name = user_dict["first_name"]
+        self.last_name = user_dict["last_name"]
+        self.dob = user_dict["dob"]
+        self.gender = user_dict["gender"]
+        self.phone = user_dict["phone"]
+        self.email = user_dict["email"]
+        self.password = user_dict["password"]
+        self.created_at = user_dict["created_at"]
+        self.updated_at = user_dict["updated_at"]
+        self.is_active = user_dict["is_active"]
+        
+        return self
+
+    def to_dict(self):
+        user_dict = {
+            "id": self.id,
+            "first_name" = self.first_name,
+            "last_name" = self.last_name,
+            "dob" = self.dob,
+            "gender" = self.gender,
+            "phone" = self.phone,
+            "email" = self.email,
+            "created_at" = self.created_at,
+            "updated_at" = self.updated_at,
+            "is_active" = self.is_active
+        }
+        return user_dict
+        
+    def from_json
 
     def get(self, dbconnection, table_name="users"):
         query = f"""SELECT * FROM {table_name} WHERE id = {self.id}""" 
@@ -56,7 +106,8 @@ class User:
         result = cursor.fetchall()
         cursor.close()
         dbconnection.close()
-        return result 
+        self.from_tuple(result)
+        return self 
 
     def update(self, dbconnection, table_name="users"):
         query = f"""UPDATE {table_name}
@@ -104,11 +155,37 @@ class UserFile:
         self.created_at = None 
         self.updated_at = None  
     
-    def insert(self):
-        pass 
+    def insert(self, dbconnection, table_name="user_files"):
+        query = f"""
+        INSERT INTO {table_name} (
+            uploaded_image_url, 
+            selfie_url, 
+            user_id, 
+            created_at, 
+            updated_at 
+        ) 
+        VALUES ( 
+            '{self.uploaded_image_url}', 
+            '{self.selfie_url}', 
+            {self.user_id},  
+            {self.created_at}, 
+            {self.updated_at}
+        );""" 
+        cursor = dbconnection.cursor()
+        cursor.execute(query)
+        dbconnection.commit()
+        cursor.close()
+        dbconnection.close()
+        
 
-    def get(self):
-        pass 
+    def get(self, dbconnection, table_name="user_files"):
+        query = f"""SELECT * FROM {table_name} WHERE id = {self.id}""" 
+        cursor = dbconnection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        dbconnection.close()
+        return result 
 
     def update(self):
         pass 
@@ -125,6 +202,7 @@ if __name__ == "__main__":
     user.last_name = "User 1 LN"
     user.email = "user1@email.com"
     user.phone = "+748777777"
+    user.is_active = 1
     user.password = "123456789"
     user.created_at = datetime.now().timestamp()
     user.updated_at = datetime.now().timestamp()
@@ -139,28 +217,42 @@ if __name__ == "__main__":
     u = user.get(connection)
     print(u) 
     
-    user.first_name = "Malcho"
-    connection = connect()
-    user.update(connection)
+    
+    file = UserFile()
+    file.uploaded_image_url = "123456"
+    file.selfie_url="83478297489237498"
+    file.user_id = 1
+    file.created_at = datetime.now().timestamp()
+    file.updated_at = datetime.now().timestamp()
     
     connection = connect()
-    u = user.get(connection)
-    print(u) 
-    
-    user.last_name = "Malchov"
+    file.insert(connection)
     connection = connect()
-    user.update(connection)
+    file.id = 1
+    f = file.get(connection)
+    print(f) 
+    # user.first_name = "Malcho"
+    # connection = connect()
+    # user.update(connection)
+    
+    # connection = connect()
+    # u = user.get(connection)
+    # print(u) 
+    
+    # user.last_name = "Malchov"
+    # connection = connect()
+    # user.update(connection)
  
-    connection = connect()
-    u = user.get(connection)
-    print(u) 
+    # connection = connect()
+    # u = user.get(connection)
+    # print(u) 
     
-    connection = connect()
-    user.deleteAll(connection)
+    # connection = connect()
+    # user.deleteAll(connection)
     
-    connection = connect()
-    u = user.get(connection)
-    print(u) 
+    # connection = connect()
+    # u = user.get(connection)
+    # print(u) 
     
     
     
--- CSE/dimitrova-niya/back-end/storage.py
+++ CSE/dimitrova-niya/back-end/storage.py
@@ -41,12 +41,13 @@ connection.close()
 import sqlite3
 
 def connect():
- return sqlite3.connect("C:\\Users\\NIYA\\Desktop\\Web applications\\rau-webapps-24-25\\CSE\\app\\back-end\\app.db")
+ return sqlite3.connect("C:\\Users\\NIYA\\Desktop\\Web applications\\rau-webapps-24-25\\CSE\\dimitrova-niya\\back-end\\app.db")
 
 connection = connect()
 
 # Database interaction example
 # 1. create a query (define)
+#create userfiles table
 query = """CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name VARCHAR,
@@ -78,4 +79,23 @@ connection.commit()
 cursor.close() 
 
 # 6. close connection (optional, ideal)
+connection.close()
+
+
+
+query = """CREATE TABLE IF NOT EXISTS user_files(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uploaded_image_url VARCHAR,
+    selfie_url VARCHAR,
+    user_id INTEGER,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+    );"""
+    
+    
+connection = connect()
+cursor = connection.cursor()
+cursor.execute(query)
+connection.commit()
+cursor.close()
 connection.close()
