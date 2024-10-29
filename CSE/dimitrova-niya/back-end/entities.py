cfb6218e838d168ff95b2ecbbe78289bfdde441e
Niya-03 <niya.dimitrova.2016@gmail.com>
2024-10-29 11:14:37 +0200

    Changed files to my own folder

--- CSE/dimitrova-niya/back-end/entities.py
+++ CSE/dimitrova-niya/back-end/entities.py
@@ -1,116 +1,166 @@
 from datetime import datetime
-from sqlite3 import connect
 
 
 class User:
-    
     def __init__(self):
-        self.id = None
-        self.first_name = None
-        self.last_name = None
+        self.id = None 
+        self.first_name = None 
+        self.last_name = None 
         self.dob = None
-        self.gender = None
-        self.phone = None
-        self.email = None
-        self.password = None
-        self.created_at = None
-        self.updated_at = None
-        self.is_active = None
-        
-    def validate_password(self):
+        self.gender = None 
+        self.phone = None 
+        self.email = None 
+        self.password = None 
+        self.created_at = None 
+        self.updated_at = None 
+    
+    def is_password_valid(self):
         if self.password is None or len(self.password) < 8:
-            return False
-        return True
+            return False 
+        return True 
     
     def insert(self, dbconnection, table_name="users"):
         query = f"""
-        INSERT INTO {table_name} (first_name,
-        last_name,
-        dob,
-        gender,
-        phone,
-        email,
-        password,
-        created_at,
-        updated_at)
-        
-        VALUES ('{self.first_name}',
-        '{self.last_name}',
-        {self.dob}, 
-        {self.gender},
-        '{self.phone}',
-        '{self.email}',
-        '{self.password}',
-        {self.created_at},
-        {self.updated_at});
-        """
+        INSERT INTO {table_name} (
+            first_name, 
+            last_name, 
+            dob, 
+            gender, 
+            phone, 
+            email, 
+            password, 
+            created_at, 
+            updated_at
+        ) 
+        VALUES (
+            '{self.first_name}', 
+            '{self.last_name}', 
+            {self.dob}, 
+            {self.gender}, 
+            '{self.phone}', 
+            '{self.email}', 
+            '{self.password}', 
+            {self.created_at}, 
+            {self.updated_at}
+        );""" 
         cursor = dbconnection.cursor()
         cursor.execute(query)
         dbconnection.commit()
         cursor.close()
         dbconnection.close()
-    
-    def get(self,dbconnection, table_name="users"):
-        query = f"""
-        SELECT * FROM {table_name} WHERE id = {self.id}
-        """
+
+    def get(self, dbconnection, table_name="users"):
+        query = f"""SELECT * FROM {table_name} WHERE id = {self.id}""" 
         cursor = dbconnection.cursor()
         cursor.execute(query)
         result = cursor.fetchall()
-        
-        print(result)
+        cursor.close()
+        dbconnection.close()
+        return result 
+
+    def update(self, dbconnection, table_name="users"):
+        query = f"""UPDATE {table_name}
+        SET first_name = '{self.first_name}', 
+            last_name = '{self.last_name}', 
+            dob = {self.dob}, 
+            gender = {self.gender}, 
+            phone = '{self.phone}', 
+            email = '{self.email}', 
+            password = '{self.password}', 
+            created_at = {self.created_at}, 
+            updated_at =  {self.updated_at}
+        WHERE id = {self.id}""" 
+        cursor = dbconnection.cursor()
+        cursor.execute(query)
         dbconnection.commit()
         cursor.close()
         dbconnection.close()
-    
-    def update(self, dbconnection, table_name="users"):
-        pass
-    
-    def delete(self, dbconnection, table_name="users"):
-        pass
-    
+          
 
-class UserFiles:
+    def delete(self, dbconnection, table_name="users"):
+        query = f"""DELETE FROM {table_name} WHERE id = {self.id}""" 
+        cursor = dbconnection.cursor()
+        cursor.execute(query)
+        dbconnection.commit()
+        cursor.close()
+        dbconnection.close()
+        print("deleted succesfully")
+        
+    def deleteAll(self, dbconnection, table_name="users"):
+        query = f"""DELETE FROM {table_name}"""
+        cursor = dbconnection.cursor()
+        cursor.execute(query)
+        dbconnection.commit()
+        cursor.close()
+        dbconnection.close()
+
+
+class UserFile:
     def __init__(self):
         self.id = None
-        self.uploaded_image_url = None
-        self.selfie_url = None
-        self.user_id = None
-        self.created_at = None
-        self.updated_at = None
-        
-    def insert(self, dbconnection, table_name="users"):
-        pass
+        self.uploaded_image_url = None 
+        self.selfie_url = None 
+        self.user_id = None 
+        self.created_at = None 
+        self.updated_at = None  
     
-    def get(self,dbconnection, table_name="users"):
-        pass
-    
-    def update(self, dbconnection, table_name="users"):
-        pass
-    
-    def delete(self, dbconnection, table_name="users"):
-        pass
-    
-    
-    
+    def insert(self):
+        pass 
+
+    def get(self):
+        pass 
+
+    def update(self):
+        pass 
+
+    def delete(self):
+        pass 
+
+
 if __name__ == "__main__":
     from storage import connect
+
     user = User()
-    user.first_name = "malcho"
-    user.last_name = "malchov"
-    user.dob = datetime.now().timestamp()
-    user.gender = 0
-    user.phone = "+63276726"
-    user.email = "malcho@gmail.com"
-    user.password = "12345678"
+    user.first_name = "User 1 FN"
+    user.last_name = "User 1 LN"
+    user.email = "user1@email.com"
+    user.phone = "+748777777"
+    user.password = "123456789"
     user.created_at = datetime.now().timestamp()
     user.updated_at = datetime.now().timestamp()
-    
+    user.dob = datetime.now().timestamp()
+    user.gender = 0
+
     connection = connect()
     user.insert(connection)
-    
+
     connection = connect()
     user.id = 1
     u = user.get(connection)
-    print(u)
+    print(u) 
+    
+    user.first_name = "Malcho"
+    connection = connect()
+    user.update(connection)
+    
+    connection = connect()
+    u = user.get(connection)
+    print(u) 
+    
+    user.last_name = "Malchov"
+    connection = connect()
+    user.update(connection)
+ 
+    connection = connect()
+    u = user.get(connection)
+    print(u) 
+    
+    connection = connect()
+    user.deleteAll(connection)
+    
+    connection = connect()
+    u = user.get(connection)
+    print(u) 
+    
+    
     
